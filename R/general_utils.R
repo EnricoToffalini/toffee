e94a22ca3a8ed693de1d20d8bd7ffbed39a87e52
EnricoToffalini <enrico.toffalini@unipd.it>
2024-06-07 14:41:57 +0200

    update

--- R/general_utils.R
+++ R/general_utils.R
@@ -44,12 +44,12 @@ try_seed = function(expr, maxrun = 10) {
 ##########################################
 
 #' Transform z-score(s) into Sum Score(s) 
-#' @description Transform z-score(s) into "plausible" Sum Score(s) that could be obtained from a test or questionnaire with a finite number of items with binomial or ordinal responses
+#' @description Transform z-score(s) into plausible Sum Score(s) that could be obtained from a test or questionnaire with a finite number of items with binomial or ordinal responses
 #' @param z A single z-score or a vector of z-scores, plausibly reflecting the "true" latent scores. Must be numeric
 #' @param nItems The number of items composing the test or questionnaire
 #' @param minResp The minimum observable response to a single item (for binomial responses 0 is recommended; for a Likert scale in 1-4, should be 1). Must be an integer number
 #' @param maxResp The maximum observable response to a single item (for binomial responses 1 is recommended; for a Likert scale in 1-4, should be 4). Must be an integer number
-#' @param itemZmin The item difficulty of the easiest item, on a z-score scale. Other item difficulties will be computed on equal interval between intemZmin and itemZmax
+#' @param itemZmin The item difficulty of the easiest item, on a z-score scale. All other item difficulties will be computed on equal intervals between this and itemZmax
 #' @param itemZmax The item difficulty of the most difficult item, on a z-score scale
 #'
 #' @return The "z" vector transformed in a vector of plausible sum scores
